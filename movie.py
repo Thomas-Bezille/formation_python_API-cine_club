0e0b6bb950d6f7820cd1f523c3e10081a6b4396d
thomas <bezille.thomasb@gmail.com>
2024-12-06 17:00:09 +0100

    📦 NEW: Creating the method to add a movie to the list

---
+++ movie.py
@@ -0,0 +1,42 @@
+import json
+import logging
+from pathlib import Path
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+CUR_DIR = Path(__file__).resolve().parent
+DATA_FILE = CUR_DIR / "data" / "movies.json"
+
+class Movie():
+    def __init__(self, title: str):
+        self.title = title.title()
+    
+    def __str__(self):
+        return f"{self.title}"
+    
+    def _get_movies(self):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    
+    def _write_movies(self, movies):
+        with open(DATA_FILE, "w") as f:
+            json.dump(movies, f, indent=4)
+    
+    def add_to_movies(self):
+        movies = self._get_movies()
+        if self.title not in movies:
+            movies.append(self.title)
+            self._write_movies(movies)
+            return True
+        else:
+            logging.warning(f"Le film {self.title} est déjà présent dans la liste.")
+            return False
+            
+            
+
+if __name__ == "__main__":
+    pass
+
+    
+    
