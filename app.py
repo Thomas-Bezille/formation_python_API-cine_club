5cda129a925064884ded2ccb7d565c9949ba4ee1
thomas <bezille.thomasb@gmail.com>
2024-12-07 17:49:27 +0100

    📦 NEW: Creating the method to add a movie

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from PySide6 import QtWidgets, QtCore
-from movie import get_all_movies
+from movie import get_all_movies, Movie
 
 class App(QtWidgets.QWidget):
     def __init__(self):
@@ -13,11 +13,13 @@ class App(QtWidgets.QWidget):
         self.layout = QtWidgets.QVBoxLayout(self) # type: ignore
         self.lne_movie_title = QtWidgets.QLineEdit()
         self.btn_add_movie = QtWidgets.QPushButton("Ajouter un film")
+        self.lbl_info = QtWidgets.QLabel("")
         self.lst_movie_list = QtWidgets.QListWidget()
         self.btn_rem_movie = QtWidgets.QPushButton("Supprimer le(s) film(s)")
         
         self.layout.addWidget(self.lne_movie_title)
         self.layout.addWidget(self.btn_add_movie)
+        self.layout.addWidget(self.lbl_info)
         self.layout.addWidget(self.lst_movie_list)
         self.layout.addWidget(self.btn_rem_movie)
     
@@ -34,7 +36,25 @@ class App(QtWidgets.QWidget):
             self.lst_movie_list.addItem(lw_item)
     
     def add_movie(self):
-        print("On ajoute un film")
+        self.lbl_info.setText("")
+        movie_to_add = self.lne_movie_title.text()
+        if not movie_to_add:
+            self.lbl_info.setText("Vous devez renseigner un titre de film.")
+            return False
+        
+        movie = Movie(movie_to_add)
+        result = movie.add_to_movies()
+        if result:
+            movie.add_to_movies()
+        
+            lw_item = QtWidgets.QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.ItemDataRole.UserRole, movie)
+            self.lst_movie_list.addItem(lw_item)
+            self.lbl_info.setText("Le film a bien été ajouté.")
+        else:
+            self.lbl_info.setText("Le film que vous voulez ajouter existe déjà.")
+        
+        self.lne_movie_title.setText("")
     
     def remove_movie(self):
         print("On supprime un film")
